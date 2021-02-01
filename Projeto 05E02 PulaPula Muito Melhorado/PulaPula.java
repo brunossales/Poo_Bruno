1dbcac63af50aac422b1942465305777a6193662
brunossales <bruno.particular25@alu.ufc.br>
2021-02-01 09:51:21 -0300

    Treinando o throw cath nesse código

--- Projeto 05E02 PulaPula Muito Melhorado/PulaPula.java
+++ Projeto 05E02 PulaPula Muito Melhorado/PulaPula.java
@@ -1,69 +1,75 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-class Crianca{
+
+class Crianca {
     String nome;
     int idade;
-    public Crianca(String nome, int idade){
+
+    public Crianca(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
     }
-    public String toString(){
+
+    public String toString() {
         return nome + ":" + idade;
     }
 }
 
-class PulaPula{
+class PulaPula {
     ArrayList<Crianca> esperando;
     ArrayList<Crianca> brincando;
-    public PulaPula(){
+
+    public PulaPula() {
         this.esperando = new ArrayList<>();
         this.brincando = new ArrayList<>();
     }
-    public void chegar(Crianca crianca){
-        if(crianca.idade <= 14)
+
+    public void chegar(Crianca crianca) {
+        if (crianca.idade <= 14)
             esperando.add(0, crianca);
-        
-        else if(crianca.idade > 14)
+
+        else if (crianca.idade > 14)
             System.out.println("Você nem é criança");
     }
 
-    //tira da fila de espera e coloca na fila brincando
-    public void entrar(){
-        if(esperando.isEmpty()){
+    // tira da fila de espera e coloca na fila brincando
+    public void entrar() {
+        if (esperando.isEmpty()) {
             System.out.println("Nenhuma criança na fila de espera");
             return;
         }
-        //obtive a crianca
+        // obtive a crianca
         Crianca primeira = esperando.get(esperando.size() - 1);
-        //retirei do vetor esperando
+        // retirei do vetor esperando
         esperando.remove(esperando.size() - 1);
-        //adicionei na primeira posicao do vetor brincando
+        // adicionei na primeira posicao do vetor brincando
         brincando.add(0, primeira);
     }
 
-    //tira da fila de brincando e coloca na fila de espera
-    public void sair(){
-        if(brincando.isEmpty()){
+    // tira da fila de brincando e coloca na fila de espera
+    public void sair() {
+        if (brincando.isEmpty()) {
             System.out.println("Nenhuma criança bricando");
-            return ;
+            return;
         }
         Crianca primeiraAbrincar = brincando.get(brincando.size() - 1);
         brincando.remove(brincando.size() - 1);
         esperando.add(0, primeiraAbrincar);
-        
+
     }
-    public Crianca remover(String nome){
-        for(Crianca obj : esperando){
-            if(obj.nome.equals(nome)){
+
+    public Crianca remover(String nome) {
+        for (Crianca obj : esperando) {
+            if (obj.nome.equals(nome)) {
                 Crianca sair = obj;
                 esperando.remove(obj);
                 System.out.println("Criança encontrada, estava na fila de espera, agora ela vai pra casa");
                 return sair;
             }
         }
-        for(Crianca objeto : brincando){
-            if(objeto.nome.equals(nome)){
-                Crianca sair  = objeto;
+        for (Crianca objeto : brincando) {
+            if (objeto.nome.equals(nome)) {
+                Crianca sair = objeto;
                 brincando.remove(objeto);
                 System.out.println("Criança encontrada, estava brincando, agora ela vai pra casa");
                 return sair;
@@ -72,44 +78,45 @@ class PulaPula{
         System.out.println("A criança não foi encontrada");
         return null;
     }
-    public void procurar(int index){
-        System.out.println("Digite 'espera' ou 'brincando' para remover a criança da posição que você digitou respectivamente");
+
+    public void procurar(int index) {
+        System.out.println(
+                "Digite 'espera' ou 'brincando' para remover a criança da posição que você digitou respectivamente");
         Scanner lerr = new Scanner(System.in);
-        while(true){
+        while (true) {
             String opp = lerr.next();
-            if(opp.equals("espera")){
-                if(esperando.size() >= index){
+            if (opp.equals("espera")) {
+                if (esperando.size() >= index) {
                     System.out.println("Criança encontrada, agora ela vai pra casa");
                     esperando.remove(index);
-                        return;
+                    return;
                 }
 
                 System.out.println("Criança não foi encontrada com essa posição na fila");
-                        return;
-            }
-            else if(opp.equals("brincando")){
-                if(brincando.size() >= index){
+                return;
+            } else if (opp.equals("brincando")) {
+                if (brincando.size() >= index) {
                     System.out.println("Criança encontrada, agora ela vai pra casa");
                     brincando.remove(index);
-                        break;
+                    break;
                 }
 
                 System.out.println("Criança não foi encontrada com essa posição de entrada no Pula-Pula");
-                        return;
-            }
-            else
+                return;
+            } else
                 System.out.println("Opção inexistente");
 
             System.out.println("");
         }
     }
-    public void fechar(){
+
+    public void fechar() {
         System.out.println("O parquinho está fechando por hoje!\nTodas Crianças da lista Espera e Brincando sairam!");
         brincando.removeAll(brincando);
         esperando.removeAll(esperando);
     }
 
-    public String toString(){
+    public String toString() {
         return "=>" + esperando + " => " + brincando;
     }
 }
--- Projeto 06E01 Topic/Topic.java
+++ Projeto 06E01 Topic/Topic.java
@@ -1,84 +1,91 @@
 import java.util.ArrayList;
 
-public class Topic{
+import javax.management.RuntimeErrorException;
+
+public class Topic {
     ArrayList<Pessoa> cadeiras;
     ArrayList<Pessoa> cadeirasPref;
-    public Topic(int qmax, int qpref){
+
+    public Topic(int qmax, int qpref) {
         cadeiras = new ArrayList<>();
         cadeirasPref = new ArrayList<>();
-        for(int i=0; i<qmax; i++){
-            if(i<qpref)
+        for (int i = 0; i < qmax; i++) {
+            if (i < qpref)
                 cadeirasPref.add(null);
-            
+
             else
                 cadeiras.add(null);
         }
     }
-    public boolean subir(Pessoa pessoa){
-        if(pessoa.getIdade() >= 80){
-            for(int i = 0; i < cadeirasPref.size(); i++){
-                if(cadeirasPref.get(i) == null){
+
+    public boolean subir(Pessoa pessoa) {
+        if (pessoa.getIdade() >= 80) {
+            for (int i = 0; i < cadeirasPref.size(); i++) {
+                if (cadeirasPref.get(i) == null) {
                     System.out.println("Tem vaga na prereferência, sente-se senhor");
                     cadeirasPref.set(i, pessoa);
                     return true;
                 }
             }
         }
-        for(int i=0; i < cadeiras.size(); i++){
-            if(cadeiras.get(i) == null){
+        for (int i = 0; i < cadeiras.size(); i++) {
+            if (cadeiras.get(i) == null) {
                 System.out.println("Tem vaga nas cadeiras normais, sente-se senhor");
                 cadeiras.set(i, pessoa);
                 return true;
             }
         }
-        for(int i = 0; i < cadeirasPref.size(); i++){
-            if(cadeirasPref.get(i) == null){
+        for (int i = 0; i < cadeirasPref.size(); i++) {
+            if (cadeirasPref.get(i) == null) {
                 System.out.println("Não tem vaga nas cadeiras normais, mas tem na preferencia, sente-se");
                 cadeirasPref.set(i, pessoa);
                 return true;
             }
         }
-        System.out.println("A topic está cheia!");
+        throw new RuntimeException("#Erro# : Topic cheiaaaa");
         return false;
     }
-    public Pessoa descer(String nome){
+
+    public Pessoa descer(String nome) {
         Pessoa pessoaAux;
-        for(int i = 0; i < cadeiras.size(); i++){
-            if(cadeiras.get(i).getNome().equals(nome)){
+        for (int i = 0; i < cadeiras.size(); i++) {
+            if (cadeiras.get(i).getNome().equals(nome)) {
                 System.out.println("Pessoa encontrada nas cadeiras normais");
                 pessoaAux = cadeiras.get(i);
                 cadeiras.remove(i);
                 return pessoaAux;
             }
         }
-        for(int i = 0; i < cadeirasPref.size(); i++){
-            if(cadeirasPref.get(i).getNome().equals(nome)){
+        for (int i = 0; i < cadeirasPref.size(); i++) {
+            if (cadeirasPref.get(i).getNome().equals(nome)) {
                 System.out.println("Pessoa encontrada na fila preferencial");
                 pessoaAux = cadeirasPref.get(i);
                 cadeirasPref.remove(i);
                 return pessoaAux;
             }
         }
-        System.out.println("Essa pessoa não está nesta topic!");
+        throw new RuntimeErrorException("#Erro# : Pessoa nao esta aquiii");
         pessoaAux = null;
         return pessoaAux;
     }
-    public void encerrar(){
+
+    public void encerrar() {
         System.out.println("Vamos encerrar por hoje!");
         cadeiras.removeAll(cadeiras);
         cadeirasPref.removeAll(cadeirasPref);
     }
-    public String toString(){
+
+    public String toString() {
         String saida = "[ ";
-        for(Pessoa pessoa : cadeirasPref){
-            if(pessoa != null)
+        for (Pessoa pessoa : cadeirasPref) {
+            if (pessoa != null)
                 saida += pessoa + " ";
-                
+
             else
                 saida += "@ ";
         }
-        for(Pessoa pessoa : cadeiras){
-            if(pessoa != null)
+        for (Pessoa pessoa : cadeiras) {
+            if (pessoa != null)
                 saida += pessoa + " ";
             else
                 saida += "= ";
